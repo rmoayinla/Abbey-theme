97ade6ee499df75d939f4c9d0023b144351ae5a5
rmoayinla <rmoayinla@yahoo.com>
2016-08-23 11:01:09 +0100

    working working

--- functions/custom-functions.php
+++ functions/custom-functions.php
@@ -21,7 +21,7 @@ function abbey_default_header(){
 	Want to hire me? Contact me
 	</div>';
 }
-add_action( "abbey_theme_before_header", "abbey_default_header" );
+add_action( "abbey_theme_before_header", "abbey_default_header" );//hook to default header; check header.php //
 
 function abbey_header_contact(){
 	echo '
